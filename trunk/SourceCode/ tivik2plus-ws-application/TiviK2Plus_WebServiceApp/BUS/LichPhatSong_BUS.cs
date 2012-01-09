17c20fa3ab80dc1ca0e5ddea303a185b47ffaa58
anhkhoahome@gmail.com <anhkhoahome@gmail.com@e3c96561-f867-6137-8ff6-46af31815a72>
2012-01-09 13:23:20 +0000

    AnhKhoa: Create KenhTV_BUS.cs, LichPhatSong_BUS.cs

---
+++ trunk/SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/BUS/LichPhatSong_BUS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace TiviK2Plus_WebServiceApp
+{
+    public class LichPhatSong_BUS
+    {
+    }
+}
