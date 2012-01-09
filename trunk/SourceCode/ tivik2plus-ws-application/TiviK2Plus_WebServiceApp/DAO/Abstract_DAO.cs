044c68b03d6dd22a426ed5c367459a4a7b51e6cd
anhkhoahome@gmail.com <anhkhoahome@gmail.com@e3c96561-f867-6137-8ff6-46af31815a72>
2012-01-09 13:21:03 +0000

    AnhKhoa: Create Abstract_DAO.cs, KenhTV_DAO.cs and LichPhatSong_DAO.cs

---
+++ trunk/SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DAO/Abstract_DAO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiviK2Plus_WebServiceApp.DAO
+{
+    public abstract class Abstract_DAO
+    {
+    }
+}
