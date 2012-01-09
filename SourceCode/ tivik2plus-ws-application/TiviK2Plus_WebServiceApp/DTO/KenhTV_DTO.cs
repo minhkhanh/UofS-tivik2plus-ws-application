bf30ab101747fa18d8cf02c923c6912ce06e2c4f
anhkhoahome@gmail.com <anhkhoahome@gmail.com@e3c96561-f867-6137-8ff6-46af31815a72>
2012-01-09 09:37:13 +0000

    AnhKhoa: Modify KenhTV_DTO.cs and LichPhatSong_DTO.cs

--- SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DTO/KenhTV_DTO.cs
+++ SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DTO/KenhTV_DTO.cs
@@ -7,5 +7,44 @@ namespace TiviK2Plus_WebServiceApp.DTO
 {
     public class KenhTV_DTO
     {
+        #region Properties
+        private int _maKenh;
+        public int MaKenh
+        {
+            get { return _maKenh; }
+            set { _maKenh = value; }
+        }
+
+        private String _tenMaKenh;
+        public String TenMaKenh
+        {
+            get { return _tenMaKenh; }
+            set { _tenMaKenh = value; }
+        }
+
+        private String _moTa;
+        public String MoTa
+        {
+            get { return _moTa; }
+            set { _moTa = value; }
+        }
+
+        private String _link;
+        public String Link
+        {
+            get { return _link; }
+            set { _link = value; }
+        }
+
+        private bool _isAvailable;
+        public bool IsAvailable
+        {
+            get { return _isAvailable; }
+            set { _isAvailable = value; }
+        }
+        #endregion
+
+        #region Methods
+        #endregion
     }
 }
--- SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DTO/LichPhatSong_DTO.cs
+++ SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DTO/LichPhatSong_DTO.cs
@@ -7,5 +7,37 @@ namespace TiviK2Plus_WebServiceApp.DTO
 {
     public class LichPhatSong_DTO
     {
+        #region Properties
+        private int _maLichPhatSong;
+        public int MaLichPhatSong
+        {
+            get { return _maLichPhatSong; }
+            set { _maLichPhatSong = value; }
+        }
+
+        private int _maKenh;
+        public int MaKenh
+        {
+            get { return _maKenh; }
+            set { _maKenh = value; }
+        }
+
+        private DateTime _ngay;
+        public DateTime Ngay
+        {
+            get { return _ngay; }
+            set { _ngay = value; }
+        }
+
+        private String _lichPhatSong;
+        public String LichPhatSong
+        {
+            get { return _lichPhatSong; }
+            set { _lichPhatSong = value; }
+        }
+        #endregion
+
+        #region Methods
+        #endregion
     }
 }
