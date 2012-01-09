f4ea7f2f9f3b4572b54427628d75f14df6e9ea05
anhkhoahome@gmail.com <anhkhoahome@gmail.com@e3c96561-f867-6137-8ff6-46af31815a72>
2012-01-09 09:27:46 +0000

    Create KenhTV_DTO.cs and LichPhatSong_DTO.cs

---
+++ trunk/SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DTO/LichPhatSong_DTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiviK2Plus_WebServiceApp.DTO
+{
+    public class LichPhatSong_DTO
+    {
+    }
+}
