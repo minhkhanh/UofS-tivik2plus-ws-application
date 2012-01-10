c53d1df5190b79187e1c48746d33fe146e3c1946
anhkhoahome@gmail.com <anhkhoahome@gmail.com@e3c96561-f867-6137-8ff6-46af31815a72>
2012-01-10 03:33:11 +0000

    AnhKhoa: Modify DAO's file - Add more function to test web service (not completed yet)

--- SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DAO/KenhTV_DAO.cs
+++ SourceCode/ tivik2plus-ws-application/TiviK2Plus_WebServiceApp/DAO/KenhTV_DAO.cs
@@ -3,15 +3,61 @@ using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
+using System.Collections.Generic;
+using System.Data.OleDb;
 
 namespace TiviK2Plus_WebServiceApp
 {
-    public class KenhTV_DAO : TiviK2Plus_WebServiceApp.DAO.Abstract_DAO
+    public class KenhTV_DAO : Abstract_DAO
     {
         #region Properties
+        private const String SQL_QUERY_GET_KENH_TV_LIST = @"SELECT MaKenh, TenMaKenh "
+                                                  + @"FROM KenhTivi ";
+                                                  //+ @"WHERE ";
         #endregion
 
         #region Methods
+        public List<KenhTV_DTO> GetKenhTVList()
+        {
+            List<KenhTV_DTO> _kenhTVList = new List<KenhTV_DTO>();
+            OleDbConnection _connection = null;
+
+            try
+            {
+                _connection = Connect();
+                OleDbCommand _command = new OleDbCommand(SQL_QUERY_GET_KENH_TV_LIST, _connection);
+                OleDbDataReader _dataReader = _command.ExecuteReader();
+                KenhTV_DTO buffer;
+                while (_dataReader.Read())
+                {
+                    buffer = new KenhTV_DTO();
+                    
+                    buffer.MaKenh = _dataReader.GetInt32(0);
+
+                    if (!_dataReader.IsDBNull(1))
+                    {
+                        buffer.TenMaKenh = _dataReader.GetString(1);
+                    }
+
+                    _kenhTVList.Add(buffer);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _kenhTVList = new List<KenhTV_DTO>();
+                return _kenhTVList;
+            }
+            finally
+            {
+                if ((_connection != null) && (_connection.State == System.Data.ConnectionState.Open))
+                {
+                    _connection.Close();
+                }
+            }
+
+            return _kenhTVList;
+        }
         #endregion
     }
 }
